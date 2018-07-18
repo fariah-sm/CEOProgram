c0d24b637683a199866eb78360e18f4d0f82482a
fariah-sm <fariahm7601@gmail.com>
2018-07-18 15:03:10 -0500

    added string file

---
+++ strings.py
@@ -0,0 +1,12 @@
+"""
+title: strings
+author: Fariah Mahmood
+date: 7/18/18 2:41 PM
+"""
+
+
+def how_eligible(essay):
+
+    if __name__ == '__main__':
+        print(how_eligible('This? "Yes." No, not really!'))
+        print(how_eligible('Really, not a compound sentence.'))
